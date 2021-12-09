27f56942cabb32f9ada367534925ea00c2cdec91
soto <z@mcac.cc>
2021-12-09 23:30:29 +0800

    重写JoinListener

--- src/main/java/com/mcatk/guildmanager/JoinListener.java
+++ src/main/java/com/mcatk/guildmanager/JoinListener.java
@@ -1,49 +1,37 @@
 package com.mcatk.guildmanager;
 
+import com.mcatk.guildmanager.models.Guild;
+import com.mcatk.guildmanager.sql.SQLManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
-    private Guilds guilds;
-
-    JoinListener() {
-        this.guilds = GuildManager.getPlugin().getGuilds();
-    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         String playerID = player.getName();
-        Guild guild = guilds.getPlayersGuild(playerID);
+        Guild guild = SQLManager.getInstance().getPlayerGuild(playerID);
         if (guild != null) {
             // 玩家有公会
             if (guild.getChairman().equals(playerID)) {
                 GuildManager.getPlugin().getServer().broadcastMessage(
-                        Msg.INFO + guild.getName() + "§2会长§e" + player.getName() + "§2已上线"
+                        Msg.INFO + guild.getGuildName() + "§2会长§e" + player.getName() + "§2已上线"
                 );
                 new Operation().giveGuildSquarePerm(playerID);
             }
             if (GuildManager.getPlugin().getServer().getOnlinePlayers() != null) {
                 StringBuilder msg = new StringBuilder(
-                        Msg.INFO + "§2您的公会" + guild.getName() + "§2现在在线玩家:"
+                        Msg.INFO + "§2您的公会" + guild.getGuildName() + "§2现在在线玩家:"
                 );
                 for (Player p : GuildManager.getPlugin().getServer().getOnlinePlayers()) {
-                    if (guild.hasPlayer(p.getName())) {
+                    if (guild.getId().equals(SQLManager.getInstance().getPlayerGuild(p.getName()).getId())) {
                         msg.append("§e").append(p.getName()).append(",");
                     }
                 }
                 player.sendMessage(msg.toString());
-            }
-            if (guild.isAdvancedPlayer(player.getName())) {
-                player.sendMessage(Msg.INFO + "§2您是公会高级成员，可以前往公会广场");
-                new Operation().giveGuildSquarePerm(playerID);
-            } else {
-                new Operation().removeGuildSquarePerm(playerID);
-            }
-            if (!guild.isMsgBoardEmpty()) {
-                player.sendMessage("§2公会留言板为空");
             }
         } else {
             // 玩家无公会
